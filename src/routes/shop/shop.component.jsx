b8d901acc3bc61b4b965f04f2da831801d91b724
Nicholas <infinitevolume@gmail.com>
2023-01-23 11:13:23 -0800

    Added category-preview / category components and Dynamic Routes

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,31 @@
-import { useContext } from 'react';
-import { ProductsContext } from '../../contexts/products.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import { Routes, Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+// import { useContext } from 'react';
+// import { CategoriesContext } from '../../contexts/categories.context';
+// import CategoryPreview from '../../components/category-preview/category-preview.component';
+
 import './shop.styles.scss';
 
 export default function Shop() {
-  const { products } = useContext(ProductsContext);
   return (
-    <div className='products-container'>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
   );
 }
+
+// export default function Shop() {
+//   const { categoriesMap } = useContext(CategoriesContext);
+//   return (
+//     <div className='shop-container'>
+//       {Object.keys(categoriesMap).map((title) => {
+//         const products = categoriesMap[title];
+//         return (
+//           <CategoryPreview key={title} title={title} products={products} />
+//         );
+//       })}
+//     </div>
+//   );
+// }
